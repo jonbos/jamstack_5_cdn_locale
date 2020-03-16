2e55e0e34249340be154412eb8bbdfd2cd120a82
Jon Bos <bos.jon@gmail.com>
2020-03-16 17:05:25 -0500

    localized pages

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,24 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require("dotenv").config();
 
-module.exports = async function() {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `http://newsapi.org/v2/everything?q=apple&from=2020-03-15&to=2020-03-15&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}`
+      `http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}`
     );
-    return response.data;
+    return { 
+        "country": country, 
+        "articles": response.data.articles };
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async function() {
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then(newsObjects => {
+    console.log('newsObjects', newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
 };
